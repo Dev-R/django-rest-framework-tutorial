b6fa698fcc469308262278a014d26dfc6931e5a3
Dev-R🌹 <rabimhd@icloud.com>
2022-07-02 01:19:16 +0800

    Added ViewSets & Routers

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,43 +2,62 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from django.urls import path, include
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
 
+'''
+    Binding ViewSets to URLs explicitly(Not needed since we use viewset)<The conventions for wiring up resources into views and urls can be handled automatically>
+    handler methods only get bound to the actions when we define the URLConf.
+    Notice how we're creating multiple views from each ViewSet class, by binding the http methods to the required action for each view.
+'''
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
+'''
+    Now that we've bound our resources into concrete views, we can register the views with the URL conf as usual.
+'''
+urlpatterns_explict = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 ])
 
-urlpatterns_old = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+'''
+    Because we're using ViewSet classes rather than View classes, we actually don't need to design the URL conf ourselves.
+    The conventions for wiring up resources into views and urls can be <handled automatically, using a Router class>. 
+'''
+# Create a router and register our viewsets with it.
+router = DefaultRouter() # The DefaultRouter class  automatically creates the API root view for us,
+router.register(r'snippets', views.SnippetViewSet,basename="snippet") # Registering the viewsets with the router is similar to providing a urlpattern. 
+router.register(r'users', views.UserViewSet,basename="user") # We include two arguments - the URL prefix for the views, and the viewset itself.
 
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-
-    path('api-auth/', include('rest_framework.urls')),
-    
-    path('', views.api_root),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
 ]
 
-urlpatterns_old = format_suffix_patterns(urlpatterns_old)
 # Uncomment for function based view
 # urlpatterns = [
 #     path('snippets/', views.snippet_list),
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -16,6 +16,8 @@ from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import action
 """
 DEFAULT: Will use snippet_list named function/class try to rename
 Class name format= ClassName_ViewType
@@ -23,8 +25,28 @@ USAGE: REMOVE _ViewType to run for class based views, for function based: uncomm
 """
 
 
+'''
 
+    ModelViewSet inherited all most all mixins so it provides default list,create,update etc.. action methods and GenericViewSet
+    i.e:
+        class ModelViewSet(mixins.CreateModelMixin,
+                                mixins.RetrieveModelMixin,
+                                mixins.UpdateModelMixin,
+                                mixins.DestroyModelMixin,
+                                mixins.ListModelMixin,
+                                GenericViewSet):1
+                """
+                A viewset that provides default create(),retrieve(),update(),
+                partial_update(), destroy() and list() actions. That are inherited from GenericViewSet, mixins
+                """
+                pass
 
+                                ---IMPORTANT---
+    ***THE URLS FOR CUSTOM ACTIONS BY DEFAULT DEPEND ON THE METHOD NAME ITSELF***
+    Using viewsets can be a really useful abstractio It helps ensure that URL conventions will be consistent across your API, minimizes the amount of code you need to write
+
+'''
+# Only used for explict not in viewsetss
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
@@ -33,20 +55,65 @@ def api_root(request, format=None):
     })
 
 
-class UserList(generics.ListAPIView):
+
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions. that constructs (UserList, UserDetail)-Generics
+    * ReadOnlyModelViewSet class automatically provide the default 'read-only' operations.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserList_Generics(generics.ListAPIView):
     # ListAPIView -> Get
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail_Generics(generics.RetrieveAPIView):
     # RetrieveAPIView -> Get by id
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
+
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions. that constructs (SnippetDetail, SnippetList, SnippetHighlight)-Generics
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    '''
+    @action decorator to create a custom action, named highlight.
+    This decorator can be used to add any custom endpoints that don't fit into the standard create/update/delete style.
+    Custom actions which use the @action decorator will respond to GET requests by default.
+     We can use the methods argument if we wanted an action that responded to POST requests.
+     i.e: @action(methods=['GET'], detail=False, url_path='get-courses')
+    '''
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
+
+
 # Comment out: if not in use,  Generic class based view
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetDetail_Generics(generics.RetrieveUpdateDestroyAPIView):
     # RetrieveUpdateDestroyAPIView ->  Get by id, put, delete
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -54,7 +121,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 # Comment out: if not in use,  Generic class based view
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList_Generics(generics.ListCreateAPIView):
       # ListCreateAPIView -> Get/Post
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -64,7 +131,7 @@ class SnippetList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-class SnippetHighlight(generics.GenericAPIView):
+class SnippetHighlight_Generics(generics.GenericAPIView):
     queryset = Snippet.objects.all()
     renderer_classes = [renderers.StaticHTMLRenderer]
 
